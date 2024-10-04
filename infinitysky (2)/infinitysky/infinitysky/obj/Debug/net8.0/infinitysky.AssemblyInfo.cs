5ac80a73293cb27871b89cdd6f3d43125cff43ac
MarinaOliveiraS <marinaoliveiradasilva2006@gmail.com>
2024-10-04 19:34:42 -0300

    Adicionando layout da view favoritos

--- infinitysky (2)/infinitysky/infinitysky/obj/Debug/net8.0/infinitysky.AssemblyInfo.cs
+++ infinitysky (2)/infinitysky/infinitysky/obj/Debug/net8.0/infinitysky.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("infinitysky")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abae1789d3fa59d7a33d5e1b1ed1bc206bb8f931")]
 [assembly: System.Reflection.AssemblyProductAttribute("infinitysky")]
 [assembly: System.Reflection.AssemblyTitleAttribute("infinitysky")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
